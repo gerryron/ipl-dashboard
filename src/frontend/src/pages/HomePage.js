f928c45699a1937e93730acf350f414e1a9f43f8
Gerryron <Gerryron@btpnsyariah.com>
2022-08-21 20:56:49 +0700

    Add Home page

---
+++ src/frontend/src/pages/HomePage.js
@@ -0,0 +1,30 @@
+import { React, useEffect, useState } from 'react';
+import { TeamTile } from '../components/TeamTile';
+
+import './HomePage.scss';
+
+export const HomePage = () => {
+  const [teams, setTeams] = useState([]);
+  useEffect(() => {
+    const fetchAllTeam = async () => {
+      const response = await fetch(`http://localhost:8080/team`);
+      const data = await response.json();
+      setTeams(data);
+    };
+
+    fetchAllTeam();
+  }, []);
+
+  return (
+    <div className="HomePage">
+      <div className="header-section">
+        <h1 className="app-name">Yhoboo IPL Dashboard</h1>
+      </div>
+      <div className="team-grid">
+        {teams.map((team) => (
+          <TeamTile teamName={team.teamName} />
+        ))}
+      </div>
+    </div>
+  );
+};
